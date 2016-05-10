193aea3af8fcbc50681fc1113ffae8fa0c469e19
Pauliina Solanne <pauliinasolanne@Pauliina-MacBook-Air.local>
2016-05-10 16:12:00 +1000

    front end gäng

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,17 +31,24 @@ angular.module('starter.controllers', [])
   $scope.outfits = [
     {
       outfit_id: 1,
-      occasion: 'first date',
+      occasion: 'First date',
       image_url: 'http://media4.popsugar-assets.com/files/2015/05/19/798/n/1922398/6ef7687e_edit_img_cover_file_15954503_1432054424_9k_-4fZ3IXJ.xxxlarge/i/How-Take-Mirror-Selfie.jpg',
       positive: '2',
       negative: '21'
     },
     {
       outfit_id: 2,
-      occasion: 'first date',
-      image_url: 'http://media4.popsugar-assets.com/files/2015/05/19/798/n/1922398/6ef7687e_edit_img_cover_file_15954503_1432054424_9k_-4fZ3IXJ.xxxlarge/i/How-Take-Mirror-Selfie.jpg',
-      positive: '2',
-      negative: '21'
+      occasion: 'Festival mood',
+      image_url: 'http://trendymods.com/wp-content/uploads/2015/09/2-How-To-Take-The-Perfect-Changing-Room-Selfie-.jpg',
+      positive: '30',
+      negative: '11'
+    },
+    {
+      outfit_id: 3,
+      occasion: 'Job interview',
+      image_url: 'https://s-media-cache-ak0.pinimg.com/236x/a5/dd/c7/a5ddc78f713e305d1448491ace98d551.jpg',
+      positive: '40',
+      negative: '5'
     }
   ];
 });
